cc5f732a22b50745beeaeff8d52179a2ffde25a7
Kenneth Sumang <kennethsumang08@gmail.com>
2022-06-02 00:15:43 +0800

    feat: added env support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:encrypted_notes/app/router/routes.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(App());
+Future main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
